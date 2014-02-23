38387773e612e66cf2437c231774f42743a7ebe6
Anehta <athena@mengcode.com>
2014-02-24 01:48:11 +0800

    继续架构了AGELUA引擎
    实现了oo机制
    增加了ASprite对象
    增加了ALayer对象
    增加了AScene对象
    修正了错误提示

--- AGE2D/entry.lua
+++ AGE2D/entry.lua
@@ -1,16 +1,3 @@
 ASetWidgetSize(640,480);
 Anehta("hehe");
 AGame("game_1");
-a = 0;
-
-function fuck(x)
-	if x > 10 then 
-		return;
-	end
-	x = x+1;
-	Anehta(x);
-	fuck(x);
-end
-
-hehe = 0;
-fuck(hehe);
